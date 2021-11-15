eaeebde87a1cca95029d909cd5cd57d35a4a7058
maicon04 <maikonmarkes11@hotmail.com>
2021-11-15 11:08:41 -0300

    Fix GlobalConfig.js

--- frontend/src/config/globalConfig.js
+++ frontend/src/config/globalConfig.js
@@ -1,8 +1,8 @@
 import Axios from 'axios';
-import { BASE_URL } from "Utils/requests";
+import { API_URL } from "Utils/requests";
 
 export const Http = Axios.create({
-    url: BASE_URL,
+    url: API_URL,
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
